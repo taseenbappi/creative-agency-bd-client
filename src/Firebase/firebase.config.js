635b18706f186f0297414155a037f9e44edc5528
Taseenul Hoque Bappi <taseen.bappi@gmail.com>
2023-07-23 13:11:48 +0600

    ENV variale added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDkW7haD4a92J5mXK2AYGUxB1GUWR9LW_4",
-    authDomain: "creative-agency-bd0.firebaseapp.com",
-    projectId: "creative-agency-bd0",
-    storageBucket: "creative-agency-bd0.appspot.com",
-    messagingSenderId: "1079517307641",
-    appId: "1:1079517307641:web:4328689a5b5b8cdad1b828"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSEGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 export default firebaseConfig;
